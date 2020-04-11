718562aeb9a962ba762987c633b6dc594d762227
Matthew Barton <mbarton@trinity.edu>
2020-04-10 22:46:39 -0500

    Add MCTS file

--- src/agent/MCTS.java
+++ src/agent/MCTS.java
@@ -1,6 +1,17 @@
 package agent;
 
-public class MCTS {
+import game.Card;
+import game.Dealer;
+import game.Move;
+
+public class MCTS extends Agent {
 
 	public MCTS() {}
+	
+	public Move getMove(Dealer dealer) { return null; }
+	public void setHand(Card c1, Card c2) {}
+	public void printHand() {}
+	public void setBlind(int blind) {}
+	public int getBetAmount(Dealer dealer) { return 0; }
+	public void printBetAmount() {}
 }
